d80f64096176d9b3033c56b32f40cf5d83224401
David <sanchez.dav90@gmail.com>
2018-08-16 00:17:12 -0500

    Add routes to index file

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import GlobalStyles from './GlobalStyles';
+import AppRoutes from "./routes";
 import Layout from './components/Layout';
 
-ReactDOM.render(
-  <Layout>
-    <App />
-  </Layout>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 registerServiceWorker();
