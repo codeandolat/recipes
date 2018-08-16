24fe18d9c13a4fd9a82dfa418cce5320cced8f00
David <sanchez.dav90@gmail.com>
2018-08-15 22:46:44 -0500

    Create a layout component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Layout from './components/Layout';
+
+ReactDOM.render(
+  <Layout>
+    <App />
+  </Layout>,
+  document.getElementById('root')
+);
 registerServiceWorker();
