4285d24bf3d1640288de296d84a7ab58cd2da908
David <sanchez.dav90@gmail.com>
2018-08-17 01:01:36 -0500

    Add semantic ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import "semantic-ui-css/semantic.min.css";
 
 import GlobalStyles from './GlobalStyles';
 import AppRoutes from "./routes";
-import Layout from './components/Layout';
 
 ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 registerServiceWorker();
